97b353a8e848a594eb2fcd7f7b196fb0c6cde85b
sjs <sjs9869@gmail.com>
2017-07-16 02:52:33 +0900

    게시판 꾸미기

--- app.js
+++ app.js
@@ -1,31 +1,79 @@
+// import modules
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
+// connect database
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+db.once("open",function (){
+  console.log("DB connected");
+});
+db.on("error",function (err){
+  console.log("DB ERROR :", err);
+});
+
+// model setting
+var postSchema = mongoose.Schema({
+  title: {type:String, required:true},
+  body: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now},
+  updatedAt: Date
+});
+var Post = mongoose.model('post',postSchema);
+// view setting
 app.set("view engine", 'ejs');
+
+// set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
-var data={count:0};
-
-app.get('/',function (req,res){
-  data.count++;
-  res.render('my_first_ejs',data);
+// set routes
+app.get('/posts',function(req,res){
+  Post.find({}).sort('-createdAt').exec(function (err,posts){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index",{data:posts});
+  });
+});// index
+app.get('/posts/new', function(req,res){
+  res.render("posts/new");
 });
-
-app.get('/reset',function (req,res){
-  data.count=0;
-  res.render('my_first_ejs',data);
+app.post('/posts',function(req,res){
+  Post.create(req.body.post,function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+});// create
+app.get('/posts/:id', function(req,res){
+  Post.findById(req.params.id, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show",{data:post});
+  });
+});//show
+app.get('/posts/:id/edit', function(req,res){
+  Post.findById(req.params.id, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit",{data:post});
+  });
 });
-
-app.get('/set/count',function (req,res){
-  if(req.query.count) data.count=req.query.count;
-  res.render('my_first_ejs',data);
-});
-
-app.get('/set/:num',function (req,res){
-  data.count=req.params.num;
-  res.render('my_first_ejs',data);
-});
+app.put('/posts/:id', function(req,res){
+  req.body.post.updatedAt=Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+});// update
+app.delete('/posts/.id', function(req,res){
+  Post.findByIdAndRemove(req.prams.id, function (err,post){
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+});// destroy
 app.listen(3001, function(){
   console.log('Server On');
 });
